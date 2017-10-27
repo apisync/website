e996c036525c137673dd0ea69002b370bf56412b
Alexandre de Oliveira <chavedomundo@gmail.com>
2017-10-26 22:36:36 -0200

    First website version

---
+++ js/app.js
@@ -0,0 +1,7 @@
+//var canvas = document.getElementById("canvas");
+//var ctx = canvas.getContext("2d");
+//ctx.setLineDash([4, 6]);/*dashes are 5px and spaces are 3px*/
+//ctx.beginPath();
+//ctx.moveTo(0,201);
+//ctx.lineTo(400, 100);
+//ctx.stroke();
